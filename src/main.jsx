6c8f89327d8d14101dc0f56add74dbfb762c544d
Mauricio Garcia <mauriciogaraco@gmail.com>
2026-05-26 09:12:09 -0500

    setup pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import { registerSW } from 'virtual:pwa-register'
+
 import { AppProvider } from './context/AppContext'
+
 import App from './App'
+
 import './index.css'
+
+// Register PWA service worker
+registerSW({
+  immediate: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
